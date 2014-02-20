07ac0ce43d787b5abc15a71388b997f1bf8d266b
BubbleNet <raquasa123@gmail.com>
2014-02-20 00:02:04 -0500

    page discovery, auth, eclipse project files
    Added page discovery (crawler), added authentication skeleton (auth),
    created eclipse project (.project, .pydevproject).

---
+++ auth.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 19, 2014
+
+@author: Dan
+'''
+import sys
+import requests
+
---
+++ crawler.py
@@ -0,0 +1,48 @@
+'''
+Created on Feb 19, 2014
+
+@author: Dan
+'''
+import urllib.request
+import urllib.parse
+from bs4 import *
+
+#root = "http://www.google.com"
+
+
+'''
+@param  | (string)        |  root - the url to be searched
+        |                 | 
+@return | (array[string]) |  pages found
+'''
+def search(root):
+    urls = [root]
+    visited = []
+    
+    while len(urls) > 0:
+        try:
+            html = urllib.request.urlopen(urls[0])
+        except:
+            print("Error accessing " + urls[0])
+        soup = BeautifulSoup(html)
+        urls.pop(0)
+        print(len(urls))
+        for i in soup.findAll('a', href = True):
+            i["href"] = urllib.parse.urljoin(root, i["href"])
+            if i["href"] not in visited and root in i["href"]:
+                urls.append(i["href"])
+                visited.append(i["href"])
+    return visited
+
+
+'''
+@param  | (array[string]), string  | visited - list of urls, string - desired file name
+        |                          | 
+@return | nothing                  |  
+'''
+def output(visited,name):
+    file = open(name,'w')
+    for i in visited:
+        file.write(i + ", ")
+    
+
