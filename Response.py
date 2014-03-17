cb68ed3200d2eb2343acc02f3c656a7828123ee8
BubbleNet <raquasa123@gmail.com>
2014-03-17 19:26:37 -0400

    almost done putting things together

--- Response.py
+++ Response.py
@@ -3,6 +3,7 @@ import urllib.parse
 from bs4 import *
 import requests
 import datetime
+from auth import Auth
 '''
 Created on Mar 16, 2014
 
@@ -18,33 +19,51 @@ class Test:
         expectedTime = datetime.timedelta(seconds=rTime)
         if response.elapsed > expectedTime:
             rs = "response to " + response.url + " took longer than expected"
-            print(rs) #testing
+            return rs
             
             
     def responseTest(self, response):
         if response.status_code != 200:
             rs = "response from" + response.url + " is " + response.checkCode()
-            print(rs) #testing
+            return rs
             
             
     def dataTest(self, response, file):
         vectors = [line.strip() for line in open(file)]
         source = response.text
+        warnings = []
+        rs = ""
         #compare text to vectors
         for i in vectors:
             j = source.find(i)
             #return warning if anything in vectors detected
             if j >= 0:
-                rs = vectors[i] + " found in " + response.url
-                print(rs) #testing
+                warnings.append(vectors[i] + " found in " + response.url)
+        for j in warnings:
+            rs = rs + j + "\n"       
+        return rs
         
         
     def sanTest(self, response):
         source = response.text
         #fucking find out how to do this
-    
-    
+        rs = ""
+        return rs
 
+"""
+"""
+def getResults(urlList, responseTime, testList, username, password):    
+    session = Auth.authenticate(urlList[0], username, password)
+    results = []
+    for i in urlList:
+        response = session.get(urlList[i])
+        test = Test(session.get(response))
+        a = test.delayTest(response, responseTime)
+        b = test.responseTest(response)
+        c = test.dataTest(response, testList)
+        d = test.dataTest(response)
+        resultsForPage = response.url + "\n" + a + "\n" + b + "\n" + c + "\n" + d + "\n"
+    return results
 
 
 
