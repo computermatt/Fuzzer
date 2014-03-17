a9a4079bb4aa044e1b4a900ad5509699cc5cf7f9
BubbleNet <raquasa123@gmail.com>
2014-03-17 11:20:36 -0400

    changed something a bit.

--- Response.py
+++ Response.py
@@ -9,30 +9,39 @@ Created on Mar 16, 2014
 @author: Dan
 '''
 
-#a = Response(requests.get("http://127.0.0.1/dvwa/vulnerabilities/sqli/"))
+class Test:
     
-def delayTest(response,rTime):
-    expectedTime = datetime.timedelta(seconds=rTime)
-    if response.elapsed > expectedTime:
-        print("response to " + response.url + " took longer than expected")
-        
-def responseTest(response):
-    if response.status_code != 200:
-        print("response from" + response.url + " is " + response.checkCode())
-        
-def dataTest(response, file):
-    vectors = [line.strip() for line in open(file)]
-    source = response.text
-    #compare text to vectors
-    for i in vectors:
-        j = source.find(i)
-        #return warning if anything in vectors detected
-        if j >= 0:
-            print(vectors[i] + " found in " + response.url)
+    def __init__(self, response):
+        self.response = response
     
-def sanTest(response):
-    source = response.text
-    #do this somehow
+    def delayTest(self, response, rTime):
+        expectedTime = datetime.timedelta(seconds=rTime)
+        if response.elapsed > expectedTime:
+            rs = "response to " + response.url + " took longer than expected"
+            print(rs) #testing
+            
+            
+    def responseTest(self, response):
+        if response.status_code != 200:
+            rs = "response from" + response.url + " is " + response.checkCode()
+            print(rs) #testing
+            
+            
+    def dataTest(self, response, file):
+        vectors = [line.strip() for line in open(file)]
+        source = response.text
+        #compare text to vectors
+        for i in vectors:
+            j = source.find(i)
+            #return warning if anything in vectors detected
+            if j >= 0:
+                rs = vectors[i] + " found in " + response.url
+                print(rs) #testing
+        
+        
+    def sanTest(self, response):
+        source = response.text
+        #fucking find out how to do this
     
     
 
