04735b9ecfab5b8140f19a2311ab4b63e0f8e2e2
BubbleNet <raquasa123@gmail.com>
2014-03-17 11:14:02 -0400

    sensitive data test done.

--- Response.py
+++ Response.py
@@ -10,26 +10,29 @@ Created on Mar 16, 2014
 '''
 
 #a = Response(requests.get("http://127.0.0.1/dvwa/vulnerabilities/sqli/"))
-
-
-def test(response):
     
-#check delay
-    expectedTime = datetime.timedelta(seconds=1)
+def delayTest(response,rTime):
+    expectedTime = datetime.timedelta(seconds=rTime)
     if response.elapsed > expectedTime:
         print("response to " + response.url + " took longer than expected")
         
-#Check response code
+def responseTest(response):
     if response.status_code != 200:
         print("response from" + response.url + " is " + response.checkCode())
         
-#Check for test data
+def dataTest(response, file):
+    vectors = [line.strip() for line in open(file)]
     source = response.text
     #compare text to vectors
-    #return warning if anything in vectors detected
+    for i in vectors:
+        j = source.find(i)
+        #return warning if anything in vectors detected
+        if j >= 0:
+            print(vectors[i] + " found in " + response.url)
     
-#Check for sanatization of input
-    source2 = response.text
+def sanTest(response):
+    source = response.text
+    #do this somehow
     
     
 
